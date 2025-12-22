f4748ee6b400867c428fe2fa907947bc0bb51005
KisanVista <kishanbm22@gmail.com>
2025-12-22 14:46:24 +0530

    Revert "Merge origin/sign-up-changes into frontend (allow unrelated histories)"
    This reverts commit 6a5a442e5e5bb84ade783f5faf49e03b57f2a405, reversing
    changes made to 991cc41ceb9c20a53e23f4ed2b8782e0b824f64e.

--- nextor-backend/dist/utils/file.util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// File utilities
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getExtension = getExtension;
-function getExtension(name) {
-    const i = name.lastIndexOf(".");
-    return i >= 0 ? name.slice(i).toLowerCase() : "";
-}
-exports.default = { getExtension };
